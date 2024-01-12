73ba1246062758ac1e6a07c5fbbb334d8412cb8e
leandro4silva <leandro94.a.s@gmail.com>
2024-01-12 13:25:46 -0300

    feat: fetch products

--- src/data/api.ts
+++ src/data/api.ts
@@ -3,7 +3,8 @@ import { RequestInit } from "next/dist/server/web/spec-extension/request";
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL;
-  const url = new URL(path, baseUrl);
+  const apiPrefix = "/api";
+  const url = new URL(apiPrefix.concat(path), baseUrl);
 
   return fetch(url, init);
 }
