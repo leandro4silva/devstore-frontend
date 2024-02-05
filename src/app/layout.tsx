0cc6bda55b6bd660eaeac72feba806ed85cfdf6a
leandro4silva <leandro94.a.s@gmail.com>
2024-02-05 16:48:59 -0300

    feat: add cart context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { CartProvider } from "@/contexts/cart-context";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt" className={inter.className}>
-      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
-    </html>
+    <CartProvider>
+      <html lang="pt" className={inter.className}>
+        <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
+      </html>
+    </CartProvider>
   );
 }
