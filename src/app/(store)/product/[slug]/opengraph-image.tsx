31faa860281ccf1a7a59205e507bb9abcc24a0ff
leandro4silva <leandro94.a.s@gmail.com>
2024-02-05 16:49:10 -0300

    feat: add opengraph

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,50 @@
+import { ImageResponse } from "next/server";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image() {
+  // Font
+  const interSemiBold = fetch(
+    new URL("./Inter-SemiBold.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        About Acme
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    },
+  );
+}
